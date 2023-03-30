7667b3ae9ece77040b18d181e3e956b904ed6474
SavyonM <124379798+SavyonMalko@users.noreply.github.com>
2023-03-30 22:27:40 +0000

    done

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@ function calculate() {
   const b = Number(document.getElementById("b").value);
   const area = Math.PI * a * b;
   const circumference = 2 * Math.PI * Math.sqrt((a * a + b * b) / 2);
-  const result = `Area: ${area.toFixed(2)}, Circumference: ${circumference.toFixed(2)}`;
+  const result = `Area: ${area.toFixed(2)}m<sup>2</sup> <br> Circumference: ${circumference.toFixed(2)}m<sup>2</sup> `;
   document.getElementById("result").innerHTML = result;
 }
